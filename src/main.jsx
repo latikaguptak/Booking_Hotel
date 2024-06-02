ea894bd134cb32c67af93ee4633810cd0733d9fd
latikaguptak <08guptal@gmail.com>
2024-06-02 22:03:29 +0530

    Frontend application creation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './assets/pages/Home/Home';
+import List from './assets/pages/Hotel_list/List';
+import Hotel from './assets/pages/Hotel_details/Hotel';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+
+  children: ([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: '/list',
+    element: <List/>,
+  },
+  {
+    path: '/list/:id',
+    element: <Hotel/>
+  }
+])
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>,
 )
