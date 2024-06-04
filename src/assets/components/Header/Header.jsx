ba5429f5326afd00e1c71c0a7c3dde2461bc0584
latikaguptak <08guptal@gmail.com>
2024-06-04 23:34:23 +0530

    Header updated and add the search icon

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,32 +1,58 @@
 import "./Header.css";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faHouse } from '@awesome.me/kit-KIT_CODE/icons/classic/solid'
-import { FaBed, FaCar, FaHotel, FaPlane, FaTaxi } from 'react-icons/fa';
+import { FaBed, FaCalendar, FaCar, FaHotel, FaPersonBooth, FaPlane, FaTaxi, FaUser, FaUsers } from 'react-icons/fa';
+import{BsPersonStanding} from 'react-icons/bs';
 const Header = () => {
   return (
     <>
       <div className="header">
         <div className="headerContainer">
           <div className="headerList">
-            <div className="headerlistitem">
+            <div className="headerlistitem active">
                 <FaBed/>
               <span> Stay</span>
             </div>
-            <div className="headerlistitem">
+            <div className="headerlistitem active">
                 <FaPlane/>
               <span> Flights </span>
             </div>
-            <div className="headerlistitem">
+            <div className="headerlistitem active">
                 <FaCar/>
               <span> Car Rentals </span>
             </div>
-            <div className="headerlistitem">
+            <div className="headerlistitem active">
                 <FaHotel/>
               <span> Attrections </span>
             </div>
-            <div className="headerlistitem">
+            <div className="headerlistitem active">
                 <FaTaxi/>
               <span> Airport Taxis </span>
+            </div>
+          </div>
+          <h1 className="headerTitle"> Life is Simple Enjoyfull</h1>
+          <p className="headerDisc">
+            Hotel Booking life ko kerde assan 
+          </p>
+          <button className="headerBtn"> Signin / Register</button>
+          <div className="headerSearch">
+            <div className="headerSearchItem">
+                <FaBed className="headerIcon"/>
+                <input type="text" className="headerSearchInput" placeholder="Where to going"/>
+            </div>
+            <div className="headerSearchItem">
+                <BsPersonStanding className="headerIcon"/>
+                <span className="headerSearchText">2 Adults 2 Childerns 1 Room</span>
+            </div>
+            <div className="headerSearchItem">
+                <FaCalendar className="headerIcon"/>
+                <span className="headerSearchText">
+                    <span className="headerSearchText">Date-to-Date </span>
+                </span>
+            </div>
+            <div className="headerSearchItem">
+                <button className="headerBtn">Search</button>
+                
             </div>
           </div>
         </div>
