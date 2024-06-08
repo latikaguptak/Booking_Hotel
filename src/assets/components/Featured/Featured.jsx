53e65b1bb9c42f0f684e335596b4e9e11fcd3e79
latikaguptak <08guptal@gmail.com>
2024-06-08 22:37:40 +0530

    feature iteas added

--- src/assets/components/Featured/Featured.jsx
+++ src/assets/components/Featured/Featured.jsx
@@ -3,7 +3,39 @@ import './featured.css'
 const Featured = () => {
   return (
     <>
-    <div className=''>
+    <div className='featured'>
+      <div className='featuredItems'>
+        <img src='' alt='' className='featuredImg'/>
+        <div className='featuredTitle'>
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+        
+      </div>
+      <div className='featuredItems'>
+        <img src='' alt='' className='featuredImg'/>
+        <div className='featuredTitle'>
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+
+      </div>
+      <div className='featuredItems'>
+        <img src='' alt='' className='featuredImg'/>
+        <div className='featuredTitle'>
+          <h1>Australia</h1>
+          <h2>123 properties</h2>
+        </div>
+
+      </div>
+      <div className='featuredItems'>
+        <img src='' alt='' className='featuredImg'/>
+        <div className='featuredTitle'>
+          <h1>London</h1>
+          <h2>123 properties</h2>
+        </div>
+
+      </div>
 
     </div>
     </>
