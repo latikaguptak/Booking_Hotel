1630998895c7131eabf259c8930eae4737345b3c
latikaguptak <08guptal@gmail.com>
2024-06-21 23:34:16 +0530

    Login page updated

--- src/assets/components/Login/Login.jsx
+++ src/assets/components/Login/Login.jsx
@@ -2,14 +2,29 @@ import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordLine } from "react-icons/ri";
 import "./Login.css";
+import { useState } from "react";
+const initialValue = {
+  Username: "",
+  Password: "",
+  
+};
 
 const Login = () => {
-  const handelSubmit = async(e) => {
+  const [formData, setFormData] = useState(initialValue);
+  const handlerChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handelSubmit =(e) => {
     e.preventDefault();
     console.log("userdata", formData);
     try {
-    const response = await axios.post("",Data)
-    console.log(response.data);
+    // const response = await axios.post("",Data)
+    // console.log(response.data);
+    console.log(formData);
     setFormData(initialValue)
     }
     catch (error) {
@@ -17,19 +32,17 @@ const Login = () => {
     }
 
   }
-  const handlerChange = (e) => {
-    console.log(e.target.value);
-  }
+  
   return (
     <div className="login">
       <h2 className="Title">Login</h2>
       <form onSubmit={handelSubmit}> 
       <div className="loginInputs">
-        <input type="text" placeholder="Username" className="inputDesign" onChange={handlerChange} required />
+        <input type="text" placeholder="Username" className="inputDesign" onChange={handlerChange} required value={formData.Username}/>
         <FaUser className="iconDisplay"/>
       </div>
       <div className="loginInputs">
-        <input type="password" placeholder="Password" className="inputDesign" onChange={handlerChange} required/>
+        <input type="password" placeholder="Password" className="inputDesign" onChange={handlerChange} required  value={formData.Password}/>
         <RiLockPasswordLine className="iconDisplay"/>
       </div>
       </form>
